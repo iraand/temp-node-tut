80201bd6d0a85f94ed4f1b39142c205a5c3e912b
Ira Andaral <iraand@bigmir.net>
2022-02-04 01:00:34 +0200

    First commit

---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+// __dirname
+// __filename
+// Modules
+// 
+
+console.log(__dirname)
+console.log(__filename)
+
+setInterval(()=>{
+    console.log("hello world!")
+},1000)
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+// local
+const secret = 'SUPER SECRET'
+// share (global)
+const john = 'John'
+const peter = 'Peter'
+
+module.exports = {john, peter}
